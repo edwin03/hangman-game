3df7d5fc8de8fe105c8872ac0b5b6215b2c8cfd0
Edwin <edwin.rubio@toshiba.com>
2026-05-15 17:11:54 -0500

    added ascii art

--- main.py
+++ main.py
@@ -1,22 +1,78 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 placeholder = ""
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-print(f"Here is the random word: {chosen_word}")
+print(chosen_word)
 for char in chosen_word:
     placeholder += "_"
 
-print(f"Place holder: {placeholder}")
+print(placeholder)
 display = placeholder
 game_over = False
+lives = 6
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 while not game_over:
     guess = input("Guess a letter: ").lower()
-
     if guess in display:
         print(f"This has already been gussed: {guess}")
     else:
@@ -26,10 +82,13 @@ while not game_over:
                 display = display[:counter] + guess + display[counter+1:]
                 counter += 1
             else:
-                print("Do nothing!")
                 counter += 1
+                print(stages[lives])
+                lives -= 1
+    print(display)
     if display == chosen_word:
-         print("You won!")
+         print("You win!")
          game_over = True
-
-    print(f"Display String: {display}")
+    if counter == 0:
+        print("You lose!")
+        game_over = True
